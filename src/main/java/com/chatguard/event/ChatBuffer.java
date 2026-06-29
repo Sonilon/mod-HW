99bbae77aaaa9da30b058f0fe9da7289224c4a51
Sonilon <120474288+Sonilon@users.noreply.github.com>
2026-06-29 22:09:16 +0300

    Delete src/main/java/com/chatguard/event/ChatBuffer.java

--- src/main/java/com/chatguard/event/ChatBuffer.java
+++
@@ -1,9 +0,0 @@
-package com.chatguard.event;
-
-import com.chatguard.util.ChatParser;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-public class ChatBuffer {
-    public static ConcurrentLinkedQueue<ChatParser.Chat> queue =
-            new ConcurrentLinkedQueue<>();
-}
